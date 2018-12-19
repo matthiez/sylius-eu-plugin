4aef750bfddfcf399968e51bad12746fd2ee498c
André Matthies <matthiez@gmail.com>
2018-12-19 11:22:08 +0100

    added BaseTypeExtension

--- src/Form/Extension/ProductTypeExtension.php
+++ src/Form/Extension/ProductTypeExtension.php
@@ -4,38 +4,13 @@ declare(strict_types=1);
 namespace Ecolos\SyliusEuPlugin\Form\Extension;
 
 use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
-use Symfony\Component\Form\AbstractTypeExtension;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormBuilderInterface;
 
-final class ProductTypeExtension extends AbstractTypeExtension
+final class ProductTypeExtension extends BaseTypeExtension
 {
     /**
      * @inheritdoc
      */
     public function getExtendedTypes(): iterable {
         return [ProductType::class];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder
-            ->add('colorants', ChoiceType::class, [
-                "expanded" => true,
-                'placeholder' => "ecolos_sylius_eu_plugin.choose",
-                'required' => false,
-                "multiple" => true,
-                'choices' => [
-                    "ecolos_sylius_eu_plugin.others" => "1",
-                    "ecolos_sylius_eu_plugin.e102" => "E102",
-                    "ecolos_sylius_eu_plugin.e104" => "E104",
-                    "ecolos_sylius_eu_plugin.e110" => "E110",
-                    "ecolos_sylius_eu_plugin.e122" => "E122",
-                    "ecolos_sylius_eu_plugin.e124" => "E124",
-                    "ecolos_sylius_eu_plugin.e129" => "E129",
-                ]
-            ]);
     }
 }
--- src/Form/Extension/ProductVariantTypeExtension.php
+++ src/Form/Extension/ProductVariantTypeExtension.php
@@ -3,41 +3,15 @@ declare(strict_types=1);
 
 namespace Ecolos\SyliusColorantsPlugin\Form\Extension;
 
-use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
+use Ecolos\SyliusEuPlugin\Form\Extension\BaseTypeExtension;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductVariantType;
-use Symfony\Component\Form\AbstractTypeExtension;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormBuilderInterface;
 
-final class ProductVariantTypeExtension extends AbstractTypeExtension
+final class ProductVariantTypeExtension extends BaseTypeExtension
 {
     /**
      * @inheritdoc
      */
     public function getExtendedTypes(): iterable {
         return [ProductVariantType::class];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder
-            ->add('colorants', ChoiceType::class, [
-                "expanded" => true,
-                'placeholder' => "ecolos_sylius_colorants_plugin.choose",
-                'required' => false,
-                "multiple" => true,
-                'choices' => [
-                    "ecolos_sylius_colorants_plugin.others" => "1",
-                    "ecolos_sylius_colorants_plugin.e102" => "E102",
-                    "ecolos_sylius_colorants_plugin.e104" => "E104",
-                    "ecolos_sylius_colorants_plugin.e110" => "E110",
-                    "ecolos_sylius_colorants_plugin.e122" => "E122",
-                    "ecolos_sylius_colorants_plugin.e124" => "E124",
-                    "ecolos_sylius_colorants_plugin.e129" => "E129",
-                ]
-            ]);
     }
 }
